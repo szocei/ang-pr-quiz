02c6e647398cc40af3aaf644caabd0b602ba3900
szocei <szocei.zoltan@gmail.com>
2021-03-29 15:38:48 +0200

    class

---
+++ src/app/model/quiz.ts
@@ -0,0 +1,11 @@
+import { Question } from "./question";
+
+export class Quiz {
+    id:number=0;
+    title:string='';
+    description:string='';
+    question:Question[];
+    active:boolean=true;
+
+
+}
---
+++ src/app/model/student.ts
@@ -0,0 +1,8 @@
+export class Student {
+    id:number=0;
+    name:string='';
+    email:string='';
+    points:number=0;
+    active:boolean=true;
+
+}
